1bf771ff3065c88240e2cfa013f5af8ad5cbb122
s00213859 <s00213859@mail.itsligo.ie>
2022-05-17 09:40:57 +0100

    Commit 1

---
+++ OODMayExam2022/RentalProperty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODMayExam2022
+{
+    public enum RentalType { House, Flat, Share}
+    class RentalProperty
+    {
+        public int ID { get; set; }
+        public string Location { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
