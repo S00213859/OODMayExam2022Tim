98f505fd83d3601abd200d2e6fc2b7776be88f92
s00213859 <s00213859@mail.itsligo.ie>
2022-05-17 09:49:27 +0100

    Commit 2

--- OODMayExam2022/RentalProperty.cs
+++ OODMayExam2022/RentalProperty.cs
@@ -6,17 +6,23 @@ using System.Threading.Tasks;
 
 namespace OODMayExam2022
 {
+    //Enum
     public enum RentalType { House, Flat, Share}
     class RentalProperty
     {
+        //Properties
         public int ID { get; set; }
         public string Location { get; set; }
         public decimal Price { get; set; }
         public string Description { get; set; }
+
+        //Constructors
+
+        //Methods
+        public void IncreaseRentByPercentage(decimal amountToIncrease)
+        {
+            Price += amountToIncrease;
+        }
     }
 
-    public void IncreaseRentByPercentage()
-    {
-
-    }
 }
