6680abdcdb37b42f6a505a2e61b27711eed48c7a
s00213859 <s00213859@mail.itsligo.ie>
2022-05-17 10:07:54 +0100

    Commit 4

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -10,11 +10,14 @@ namespace TestProject1
         public void Test_Increase_By_Percentage()
         {
             //Arrange
-            RentalProperty rp1 = new RentalProperty();
+            RentalProperty rp1 = new RentalProperty() { ID = 1, Price = 40 };
+            decimal expectedRent = 50;
 
             //Act
+            rp1.IncreaseRentByPercentage(10);
 
             //Assert
+            Assert.AreEqual(expectedRent, rp1.Price);
         }
     }
 }
